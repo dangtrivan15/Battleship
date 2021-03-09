9a103f435f1b6ed00b10193d97aa54313e98ecf4
dangtrivan15 <dangtrivan15@gmail.com>
2021-03-09 23:14:58 +0700

    Ocean and Ship classes enhancement.

---
+++ src/Ocean.java
@@ -0,0 +1,44 @@
+
+public class Ocean {
+	Ship[][] ships = new Ship[20][20];
+	int shotsFired;
+	int hitCount;
+	int shipsSunk;
+	
+	public void placeAllShipsRandomly() {
+		
+	}
+	
+	public boolean isOccupied(int row, int column) {
+		
+	}
+	
+	public boolean shootAt(int row, int column) {
+		
+	}
+	
+	public int getShotsFired() {
+		
+	}
+	
+	public int getHitCount() {
+		
+	}
+	
+	public int getShipsSunk() {
+		
+	}
+	
+	public boolean isGameOver() {
+		
+	}
+	
+	public Ship[][] getShipArray() {
+		
+	}
+	
+	public void print() {
+		
+	}
+	
+}
--- src/Ship.java
+++ src/Ship.java
@@ -1,11 +1,13 @@
 
 public abstract class Ship {
+	// Attributes
     private int bowRow;
     private int bowColumn;
     private int length;
     private boolean horizontal;
     private boolean[] hit;
-
+    
+    //Getter and Setter
     public int getBowRow() {
         return bowRow;
     }
@@ -49,8 +51,40 @@ public abstract class Ship {
     
     
     
-    
+    //Methods
     abstract String getShipType();
     
+    public boolean okToPlaceShipAt(int row, int column, boolean horizontal, Ocean ocean) {
+    	if (horizontal) {
+    		for (int i = 0; i < length; i++) {
+    			if (!ocean.ships[row][column + i].getShipType().equals("empty")) {
+    				return false;
+    			}
+    		}
+    	} else {
+    		for (int i = 0; i < length; i++) {
+    			if (!ocean.ships[row + i][column].getShipType().equals("empty")) {
+    				return false;
+    			}
+    		}
+    	}
+    	return true;
+    }
+    
+    void placeShipAt(int row, int column, boolean horizontal, Ocean ocean) {
+    	if (horizontal) {
+    		for (int i = 0; i < length; i++) {
+    			ocean.ships[row][column + i] = this;
+    		}
+    	} else {
+    		for (int i = 0; i < length; i++) {
+    			ocean.ships[row + i][column] = this;
+    		}
+    	}
+    }
+    
+    boolean shootAt(int row, int column) {
+    	if (!ocean.ships[row][column].getShipType().equals("empty"))
+    }
     
 }
